9908497bc448036efea25adc93341017a4a11283
Nate <Natesky9@gmail.com>
2025-09-07 19:06:00 -0400

    moved Rendertype into a client class to allow it to run on server. Added a distance check to the glow outline of the grave

--- src/main/java/com/natesky9/catalystgraves/CatalystGraves.java
+++ src/main/java/com/natesky9/catalystgraves/CatalystGraves.java
@@ -16,7 +16,6 @@ import java.util.OptionalDouble;
 public class CatalystGraves {
 
     public static final String MODID = "catalystgraves";
-    public static RenderType SPECTRAL;
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -29,16 +28,6 @@ public class CatalystGraves {
         CGBlockEntities.register(modEventBus);
 
         //
-        SPECTRAL =  RenderType.create("test",
-                DefaultVertexFormat.POSITION_COLOR_NORMAL,
-                VertexFormat.Mode.LINES,
-                1536,
-                RenderType.CompositeState.builder()
-                        .setShaderState(RenderStateShard.RENDERTYPE_LINES_SHADER)
-                        .setLineState(new RenderStateShard.LineStateShard(OptionalDouble.of(4)))
-                        .setDepthTestState(RenderStateShard.NO_DEPTH_TEST)
-                        .setCullState(RenderStateShard.NO_CULL)
-                        .createCompositeState(false));
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(net.neoforged.fml.config.ModConfig.Type.COMMON, CGConfig.SPEC);
